4195490798a1feb528855bb313d3d719f6e0e401
Uanderson Ferreira de Oliveira <uanderson.oliveira@estudante.ifto.edu.br>
2024-05-26 21:13:51 -0300

    Initial commit

---
+++ src/main/java/br/com/uanderson/santanderdevweek/Application.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.santanderdevweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbooSantanderDevWeek2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbooSantanderDevWeek2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/uanderson/santanderdevweek/ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.uanderson.santanderdevweek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbooSantanderDevWeek2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
